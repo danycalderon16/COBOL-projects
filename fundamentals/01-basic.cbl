19f71601a041371c0c9c192c7fba61ee9117a36d
Daniel Calderon <danycalvir@gmail.com>
2024-01-27 18:17:35 -0700

    init repo

---
+++ fundamentals/01-basic.cbl
@@ -0,0 +1,27 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BASIC.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 NUM1 PIC 9(4).
+       01 NUM2 PIC 9(4).
+       01 RESULTADO PIC 9(4).
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Introduce el primer numero: ".
+            ACCEPT NUM1.
+            DISPLAY "Introduce el segundo numero: ".
+            ACCEPT NUM2.
+      * ADD NUM1 TO NUM2 GIVING RESULTADO.
+      * SUBTRACT NUM1 FROM NUM2 GIVING RESULTADO .
+      * MULTIPLY NUM1 BY NUM2 GIVING RESULTADO.
+            DIVIDE NUM1 BY NUM2 GIVING RESULTADO.
+            DISPLAY RESULTADO.
+            STOP RUN.
+       END PROGRAM BASIC.
